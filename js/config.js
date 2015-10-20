21f1c56118f1d7b9cb4e15a93e17c6b50a747a7e
409903113@qq.com <409903113@qq.com>
2015-10-20 16:42:52 +0800

    设置baseUrl属性

--- js/config.js
+++ js/config.js
@@ -1,9 +1,11 @@
 var require = {
+    baseUrl: 'js',
     paths:{
         jquery1: 'lib/jQuery'
     }        
 };
 
-
-//给require对象配置paths属性
+//设置require的baseUrl为'js'
+//找data-main是，写data-main='indexMain'
+//在indexMain，引入ui1.js模块，引入模块名'ui/ui1'
 
--- js/indexMain.js
+++ js/indexMain.js
@@ -1,18 +1,6 @@
 require([
-    'module1'
+    'ui/ui1'
 ],function(module1){
     console.log(module1);
 });
 
-
-//1.这里用的module1.js  module已经依赖注入
-//2.没有指定require对象的baseUrl，baseUrl为subPath= 'js/'
-/*
-if (!cfg.baseUrl) {
-    //Pull off the directory of data-main for use as the
-    //baseUrl.
-    src = mainScript.split('/');
-    mainScript = src.pop();
-    subPath = src.length ? src.join('/')  + '/' : './';
-    cfg.baseUrl = subPath;      
-}*/
