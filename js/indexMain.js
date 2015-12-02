bd5437d153c85b94e2e0b9326f7ca720300baf64
409903113@qq.com <409903113@qq.com>
2015-12-02 18:08:46 +0800

    domReady利用模块加载器!

--- js/indexMain.js
+++ js/indexMain.js
@@ -1,13 +1,9 @@
 require([
-    'common/domReady'
+    'common/domReady!'
 ],function(domReady){
     //相当于document.ready方法
-    domReady(function(){
-        var oDiv = document.getElementById('div1');
-        if(!oDiv){
-            debugger
-        }
-        console.log(oDiv);
-    });
+    console.log(domReady)       //document
 });
 
+//1.可以让回调函数在document加载后加载
+//2.返回值是当前document对象
