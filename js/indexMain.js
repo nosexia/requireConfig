53424187352a2cd7d0f05f9f991bd1298fa07a26
409903113@qq.com <409903113@qq.com>
2015-12-02 17:57:49 +0800

    测试domReady.js

--- js/indexMain.js
+++ js/indexMain.js
@@ -1,9 +1,13 @@
 require([
-    'backbone',
-    'jquery.dragsort',
-    'angular',
-    'common/params'
-],function(backbone, jqueryDragsort, angular, params){
-    console.log(params);
+    'common/domReady'
+],function(domReady){
+    //相当于document.ready方法
+    domReady(function(){
+        var oDiv = document.getElementById('div1');
+        if(!oDiv){
+            debugger
+        }
+        console.log(oDiv);
+    });
 });
 
